d60da60604042a563c9b692020d56bfbd4eb0421
Sam McKay <sammckay31@gmail.com>
2025-04-18 16:17:48 -0600

    set up the access to the database and the schema so we can interact with the nocomembers table in the supabase db

---
+++ src/db/schema.ts
@@ -0,0 +1,8 @@
+import { pgTable, text, uuid } from "drizzle-orm/pg-core";
+
+export const users = pgTable("nocomembers", {
+  id: uuid("id").defaultRandom().primaryKey(),
+  name: text("name").notNull(),
+  email: text("email"),
+  phoneNumber: text("phonenumber"),
+});
